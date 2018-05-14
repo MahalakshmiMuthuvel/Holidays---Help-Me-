d5911430de3b0feb3ba9711f8eaa28dea78ed180
MahalakshmiMuthuvel <37685251+MahalakshmiMuthuvel@users.noreply.github.com>
2018-05-14 08:23:56 +0530

    Chck Comments

---
+++ Core Lab/js/TopNav.js
@@ -0,0 +1,21 @@
+/*window.onscroll=function()
+{
+	scrollFunction()
+};
+function scrollFunction()
+{
+	if(document.body.scrollTop > 20 ||document.Element.scrollTop > 20)
+	{
+		document.getElementById("Arrow").style.display="block";
+	}
+	else
+	{
+		document.getElementById("Arrow").style.display="none";
+	}
+}
+function topFunction()
+{
+	document.body.scrollTop=0;
+	document.documentElement.scrollTop=0;
+}*/
+		
