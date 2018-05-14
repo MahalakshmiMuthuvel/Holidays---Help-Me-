ad689587a43bb33f0dc7d73a833d54b8d7d40e6f
= <34975833+arjunsundaram4@users.noreply.github.com>
2018-05-14 13:46:54 +0530

    Push up btn

--- Core Lab/js/TopNav.js
+++ Core Lab/js/TopNav.js
@@ -18,4 +18,24 @@ function topFunction()
 	document.body.scrollTop=0;
 	document.documentElement.scrollTop=0;
 }*/
-		
+// ===== Scroll to Top ==== 
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
